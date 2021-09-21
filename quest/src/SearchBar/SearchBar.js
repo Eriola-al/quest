063159fcc076a51452ba55f37cc8fae0b820b40f
Eriola <endoj22@gmail.com>
2021-09-21 10:57:20 +0200

    NavigationBar Added

--- quest/src/SearchBar/SearchBar.js
+++ quest/src/SearchBar/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
 
-export function SearchBar() {
+export function SearchBar(props) {
+    const sizeClass = props.small ? '' : 'is-medium';
     return (
         <div>
             <div className="field has-addons">
                 <p className="control"> 
-                    <button className="button is-static is-medium">Find</button>
+                    <button className={`button is-static ${sizeClass}`}>Find</button>
                 </p>
                 <p className="control">
-                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="takeout,plumbers,barbers..." />
+                    <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="takeout,plumbers,barbers..." />
                 </p>
                 <p className="control"> 
-                    <button className="button is-static is-medium">NEAR</button>
+                    <button className={`button is-static ${sizeClass}`}>NEAR</button>
                 </p>
                 <p className="control">
-                    <input className={`input is-medium ${styles['input-control']}`} type="text" placeholder="Where" />
+                    <input className={`input ${sizeClass} ${styles['input-control']}`} type="text" placeholder="Where" />
                 </p>
-                <div className={`button is-medium ${styles['search-button']}`}>
+                <div className={`button ${sizeClass} ${styles['search-button']}`}>
                     <span className={`icon is-small ${styles['search-icon']}`}><i className="fas fa-search"></i></span>
                 </div>
             </div>
