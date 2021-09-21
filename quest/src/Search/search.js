d0ef06762298b0f7477166381e2a284d00cd46a2
Eriola <endoj22@gmail.com>
2021-09-22 00:14:52 +0200

    SearchResults Added

--- quest/src/Search/search.js
+++ quest/src/Search/search.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { NavBar } from '../NavBar/NavBar';
 import { SubNav } from '../NavBar/SubNav/SubNav';
+import { SearchResults } from './SearchResults/SearchResults';
 
 export function Search() {
     return (
         <div>
             <NavBar />
             <SubNav />
+            <SearchResults />
         </div>
     );
 }
