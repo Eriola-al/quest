b603cd54a1f445c225de547a594df8806d57128b
Eriola <endoj22@gmail.com>
2021-09-21 14:14:49 +0200

    Sub-NavigationBar Added

--- quest/src/Search/search.js
+++ quest/src/Search/search.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { NavBar } from '../NavBar/NavBar';
+import { SubNav } from '../NavBar/SubNav/SubNav';
 
-export function Search(){
+export function Search() {
     return (
-        <div><NavBar/></div>
+        <div>
+            <NavBar />
+            <SubNav />
+        </div>
     );
 }
