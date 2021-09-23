b74ad163aca0b50bcf738b934a2904c381bcc79f
Eriola <endoj22@gmail.com>
2021-09-23 12:34:33 +0200

    API Creation/Configuartion

---
+++ quest/src/hooks/yelp-api/config.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = 'https://api.yelp.com/v3';
+
+//WARNING!Not for production use.
+//Note:Must be saved on a server.
+export const BEARER_TOKEN = 'ZlYgtOyo0iWQro-qs5O6uUhnoHSXLrU3bPiHyCktrZgyvJ1LztFqEQpa15mrL7ztOITPCZdudUfk9twaSDZrGucje7zLGtZq3vjkIeB_VCf2bZ8wmdsUZReoRkVMYXYx';
